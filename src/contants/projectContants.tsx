47d29f0b37c278c21557186bec270c2edf1e3571
rhozelly <antoniarhozelita@gmail.com>
2023-09-21 13:54:40 +0800

    force import images

--- src/contants/projectContants.tsx
+++ src/contants/projectContants.tsx
@@ -1,10 +1,39 @@
+
+import hemsImage from  '@/assets/screenshots/hems-featured.png';
+import hemsDescription from  '@/assets/screenshots/hems-description-image.png';
+import hemsOverview from  '@/assets/screenshots/hems-overview-image.png';
+
+import sleImage from  '@/assets/screenshots/sle-featured.png';
+import sleDescription from  '@/assets/screenshots/sle-description-image.png';
+import sleOverview from  '@/assets/screenshots/sle-overview-image.png';
+
+import tpImage from  '@/assets/screenshots/tp-featured.png';
+import tpDescription from  '@/assets/screenshots/tp-description-image.png';
+import tpOverview from  '@/assets/screenshots/tp-overview-image.png';
+
+import originalImage from  '@/assets/screenshots/original-content-featured.jpg';
+import originalDescription from  '@/assets/screenshots/original-content-description-image.png';
+import originalOverview from  '@/assets/screenshots/original-content-overview-image.png';
+
+import dimpleImage from  '@/assets/screenshots/dimple-cloud-featured.png';
+import dimpleDescription from  '@/assets/screenshots/dimple-cloud-description-image.png';
+import dimpleOverview from  '@/assets/screenshots/dimple-cloud-overview.png';
+
+import liveImage from  '@/assets/screenshots/dimple-live-featured.png';
+import liveDescription from  '@/assets/screenshots/dimple-live-description-image.png';
+import liveOverview from  '@/assets/screenshots/dimple-live-overview.png';
+
+import digigoozImage from  '@/assets/screenshots/digigooz-featured.png';
+import digigoozDescription from  '@/assets/screenshots/digigooz-description-image.png';
+import digigoozOverview from  '@/assets/screenshots/digigooz-overview.png';
+
 export const PROJECTS = [
     {
         id: 1,
         project_name: 'HEMS (LMS)',
         cat: 'software',
-        project_image: "/src/assets/screenshots/hems-featured.png",
-        project_description_image: '/src/assets/screenshots/hems-description-image.png',
+        project_image: hemsImage,
+        project_description_image: hemsDescription,
         project_description_content: [
             'User Management',
             'Integration and Compatibility',
@@ -14,15 +43,15 @@ export const PROJECTS = [
             'Training for Payroll Personnel',
         ],
         project_description_sub_content: 'Discover the Future of Learning, boost engagement, and achieve unprecedented results. Discover the Future of Education Today!',
-        project_overview_image: '/src/assets/screenshots/hems-overview-image.png',
+        project_overview_image: hemsOverview,
         project_overview_content: '"Empower your workforce with our cutting-edge Learning Management System. Elevate skills, drive growth!”'
     },
     {
         id: 2,
         project_name: 'SLE',
         cat: 'software',
-        project_image: '/src/assets/screenshots/sle-featured.png',
-        project_description_image: '/src/assets/screenshots/sle-description-image.png',
+        project_image: sleImage,
+        project_description_image: sleDescription,
         project_description_content: [
             'Adaptive Curriculum',
             'Grammar and Vocabulary',
@@ -31,15 +60,15 @@ export const PROJECTS = [
             'Tutor Sessions',
         ],
         project_description_sub_content: 'Ultimate English vocabulary enhancement app, meticulously designed to boost your word power and comprehension',
-        project_overview_image: '/src/assets/screenshots/hems-overview-image.png',
+        project_overview_image: sleOverview,
         project_overview_content: '"Elevate your language skills and unlock a world of opportunities with a rich, diverse dictionary.”'
     },
     {
         id: 3,
         project_name: 'TP',
         cat: 'software',
-        project_image: '/src/assets/screenshots/tp-featured.png',
-        project_description_image: '/src/assets/screenshots/tp-description-image.png',
+        project_image: tpImage,
+        project_description_image: tpDescription,
         project_description_content: [
             'Adaptive Curriculum',
             'Grammar and Vocabulary',
@@ -48,51 +77,51 @@ export const PROJECTS = [
             'Tutor Sessions',
         ],
         project_description_sub_content: 'Cutting-edge language learning application designed to help users master English with ease and confidence.',
-        project_overview_image: '/src/assets/screenshots/hems-overview-image.png',
+        project_overview_image: tpOverview,
         project_overview_content: '"Connect with native English speakers, practice in authentic situations, and gain the confidence you need to communicate effectively.”'
     },
     {
         id: 4,
         project_name: 'Original Content',
         cat: 'content',
-        project_image: '/src/assets/screenshots/original-content-featured.jpg',
-        project_description_image: '/src/assets/screenshots/original-content-description-image.png',
+        project_image: originalImage,
+        project_description_image: originalDescription,
         project_description_content: 'Elevate your online presence with our expert content creation services. Explore the art of crafting unique and engaging content tailored to your brand.',
         project_description_sub_content: 'Discover the Future of Learning, boost engagement, and achieve unprecedented results. Discover the Future of Education Today!',
-        project_overview_image: '/src/assets/screenshots/original-content-overview-image.png',
+        project_overview_image: originalOverview,
         project_overview_content: 'We are your dedicated team of content creators, leveraging the power of CMS software to craft original'
     },
     {
         id: 5,
         project_name: 'Dimple Cloud',
         cat: 'landing page',
-        project_image: '/src/assets/screenshots/dimple-cloud-featured.png',
-        project_description_image: '/src/assets/screenshots/dimple-cloud-description-image.png',
+        project_image: dimpleImage,
+        project_description_image: dimpleDescription,
         project_description_content: 'We take pride in transforming visions into extraordinary digital experiences. Our recent collaboration with Dimple Cloud stands as a testament to our expertise in web design.',
         project_description_sub_content: '',
-        project_overview_image: '/src/assets/screenshots/dimple-cloud-overview.png',
+        project_overview_image: dimpleOverview,
         project_overview_content: ''
     },
     {
         id: 6,
         project_name: 'Dimple Live',
         cat: 'landing page',
-        project_image: '/src/assets/screenshots/dimple-live-featured.png',
-        project_description_image: '/src/assets/screenshots/dimple-live-description-image.png',
+        project_image: liveImage,
+        project_description_image: liveDescription,
         project_description_content: 'We take pride in transforming visions into extraordinary digital experiences. Our recent collaboration with Dimple Cloud stands as a testament to our expertise in web design.',
         project_description_sub_content: '',
-        project_overview_image: '/src/assets/screenshots/dimple-live-overview.png',
+        project_overview_image: liveOverview,
         project_overview_content: ''
     },
     {
         id: 7,
         project_name: 'Digigooz',
         cat: 'landing page',
-        project_image: '/src/assets/screenshots/digigooz-featured.png',
-        project_description_image: '/src/assets/screenshots/digigooz-featured-description-image.png',
+        project_image: digigoozImage,
+        project_description_image: digigoozDescription,
         project_description_content: 'We take pride in transforming visions into extraordinary digital experiences. Our recent collaboration with Dimple Cloud stands as a testament to our expertise in web design.',
         project_description_sub_content: '',
-        project_overview_image: '/src/assets/screenshots/digigooz-overview.png',
+        project_overview_image: digigoozOverview,
         project_overview_content: ''
     },
 ]
