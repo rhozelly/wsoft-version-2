357956e1c5669356fa5e5038ea4f91da2bdcdd70
rhozelly <antoniarhozelita@gmail.com>
2023-12-26 17:27:55 +0800

    fixed staging icon

---
+++ src/contants/iconServicesConstants.tsx
@@ -0,0 +1,34 @@
+
+import icon1 from '@/assets/services/wd-icon-1.png';
+import icon2 from '@/assets/services/wad-icon-1.png';
+import icon3 from '@/assets/services/ui-icon-1.png';
+import icon4 from '@/assets/services/ai-icon-1.png';
+import icon5 from '@/assets/services/services-1.png';
+
+export const SERVICES_ICON = [
+    {
+        img: icon1,
+        title: "웹사이트 개발",
+        content: "모든 규모의 기업을 위한 전문 웹 개발 서비스"
+    },
+    {
+        img: icon2,
+        title: "웹 애플리케이션 개발",
+        content: "귀하의 비즈니스를 강화하는 혁신적인 웹 앱 솔루션"
+    },
+    {
+        img: icon3,
+        title: "UI 디자인",
+        content: "더 나은 디지털 세상을 위한 직관적이고 사용자 친화적인 인터페이스 제작"
+    },
+    {
+        img: icon4,
+        title: "인공지능 활용 서비스",
+        content: "고객 서비스 비용 절감, 응답 시간 개선, 고객 만족도 향상"
+    },
+    {
+        img: icon5,
+        title: "유지보수",
+        content: "우리는 웹사이트 유지 관리를 쉽고 번거롭지 않게 만듭니다."
+    }
+];
